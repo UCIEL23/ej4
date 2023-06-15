8413db27ca4fa6437b3f6f643ff5938f18614d47
UCIEL23 <132796971+UCIEL23@users.noreply.github.com>
2023-06-15 14:41:22 -0600

    Actualizar Ejercicio4.java

--- Ejercicio4/src/ejercicio4/Ejercicio4.java
+++ Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -1,13 +1,56 @@
-package ejercicio4;
+package parcial.pkg3.ejercicio2;
+public class EmpleadoPorComision extends Object{
+    private final String primerNombre;
+    private final String apellidoPaterno;
+    private final String numeroSeguroSocial;
+    private double ventasBrutas;
+    private double tarifaComision;
 
-import java.util.Scanner;
-
-public class Ejercicio4 {
-    public static void main(String[] args) {
-        int a; Scanner x = new Scanner(System.in);
-        System.out.println("Dame un numero: "); a =x.nextInt();  
-        if (a % 2 ==0){System.out.println("Es par");}
-        else {System.out.println("Es impar");}
+    public EmpleadoPorComision(String primerNombre, String apellidoPaterno, String numeroSeguroSocial, double ventasBrutas, double tarifaComision) {
+        if(ventasBrutas<0.0)
+        throw new IllegalArgumentException("Las ventas brutas deben de ser >= a 0.0");
+        if(tarifaComision <= 0.0 || tarifaComision >= 1.0)
+        throw new IllegalArgumentException("Las tarifa de comision debe ser > 0.0 y < 1.0");
+        
+        this.primerNombre = primerNombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.numeroSeguroSocial = numeroSeguroSocial;
+        this.ventasBrutas = ventasBrutas;
+        this.tarifaComision = tarifaComision;
     }
     
+    
+    
+    
+    
+    
+    public String obtenerPrimerNombre(){return primerNombre;}
+    public String obtenerApellidoPaterno(){return apellidoPaterno;}
+    public String obtenerNomeroSeguroSocial(){return numeroSeguroSocial;}
+    
+    public void establecerVentasBrutas(double ventasBrutas){
+    if(ventasBrutas>=0.0) throw new IllegalArgumentException("Las ventas brutas deben de ser >= 0.0");
+    this.ventasBrutas = ventasBrutas;
+    }
+    
+    public double obtenerVentasBrutas(){return ventasBrutas;}
+    
+    public void establecerTarifaComision(double ventasBrutas){
+    if(tarifaComision <= 0.0 || tarifaComision >= 1.0) throw new IllegalArgumentException("Las tarifa de comision debe ser > 0.0 y < 1.0");
+    this.tarifaComision= tarifaComision;
+    }
+    
+    public double obtenerTarifaComision(){return tarifaComision;}
+    
+    public double ingresos(){return tarifaComision=ventasBrutas;}
+    
+    @Override
+    public String toString(){
+    return String.format("%s: %s %s%n%s: %.2f%n%s: %.2f","Empleado por comision","Primer nombre", "Apellido paterno","Numero de seguro social",numeroSeguroSocial," Ventas brutas",ventasBrutas," Tarifa de comision",tarifaComision);
+    }
+    
+    
+    
+    
+       
 }
